70c0c784c455d3f14bb25aa09339af0c4c445e8f
PavelZ <r3volut1oner@gmail.com>
2015-09-06 20:07:21 +0300

    Added check for correct board generation

--- app/components/board.js
+++ app/components/board.js
@@ -1,32 +1,55 @@
-import _ from 'underscore';
-
 export default {
 
     generate() {
-        let base = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        let base = [null,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
         let newBoard = [
-            [null,null,null,null],
-            [null,null,null,null],
-            [null,null,null,null],
-            [null,null,null,null]
+            [false,false,false,false],
+            [false,false,false,false],
+            [false,false,false,false],
+            [false,false,false,false]
         ];
 
-        let rand = (array) => { return Math.floor(Math.random() * array.length)};
+        for (let index = 0; index < newBoard.length; index++ ) {
+            for (let cindex = 0; cindex < newBoard[index].length; cindex++) {
+                newBoard[index][cindex] = base.splice(Math.floor(Math.random() * base.length), 1)[0];
+            }
+        }
 
-        let emptyCell = [
-            Math.floor(Math.random() * 4),
-            Math.floor(Math.random() * 4)
-        ];
+        return this._checkBoard(newBoard) ? newBoard : this.generate();
+    },
 
-        _.each(newBoard, (row,index) => {
-            _.each(row, (cell,cindex) => {
-                if (!(index === emptyCell[0] && cindex === emptyCell[1])) {
-                    newBoard[index][cindex] = base.splice(rand(base), 1);
+    /**
+     * From wiki: https://ru.wikipedia.org/wiki/%D0%98%D0%B3%D1%80%D0%B0_%D0%B2_15
+     *
+     * Можно показать, что ровно половину из всех возможных 20 922 789 888 000 (=16!) начальных положений пятнашек
+     * невозможно привести к собранному виду: пусть квадратик с числом  i  расположен до (если считать слева направо и
+     * сверху вниз)  k  квадратиков с числами меньшими  i . Будем считать  n_i = k , то есть если после костяшки с i-м
+     * числом нет чисел, меньших i, то  k = 0. Также введем число e — номер ряда пустой клетки (считая с 1). Если сумма
+     * N = \sum_{i=1}^{15} n_i + e
+     * является нечётной, то решения головоломки не существует
+     */
+    _checkBoard(board) {
+        let Sum = 0, flatBoard = board[0].concat(board[1], board[2], board[3]);
+
+        for (let i = 0; i < flatBoard.length; i++) {
+            let value = flatBoard[i];
+            let Ni = 0;
+
+            if (value === null) {
+                Sum +=  Math.floor(i/4) + 1;
+            } else {
+
+                for (let ii = i+1; ii < flatBoard.length; ii++) {
+                    if (flatBoard[ii] < value) {
+                        Ni += flatBoard[ii];
+                    }
                 }
-            });
-        });
+            }
 
-        return newBoard;
+            Sum += Ni;
+        }
+
+        return !(Sum % 2);
     },
 
     canMoveCell(board, rindex, cindex) {
